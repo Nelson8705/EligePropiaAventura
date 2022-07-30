3056710030f330697444b7962eba9800a522ddf6
nelson_uriel <nelson8705@hotmail.com>
2022-07-30 18:28:44 -0300

    Se modifica estructura, se corrige bucle con condición

--- js/eligeAventura.js
+++ js/eligeAventura.js
@@ -1,35 +1,31 @@
 
 
 
-let nombre = prompt("Ingresá tu nombre")
-alert("Hola, " + nombre + ". ¿Estás list@ para comenzar con la ventura?");
+let nombre = prompt("Ingresa el nombre de tu personaje")
+alert("Hola, " + nombre + ". ¿Estás list@ para comenzar con la aventura?");
 
 
-alert(nombre + ", estás en la ruta. Siempre pensaste en la idea de escapar de la gran ciudad, instalarte en un pueblo y vivir una vida más relajada. ¿Cuál es ese nuevo Edén? Chascomús. Un sitio que, si bien está a unos pocos kilómetros de Capital Federal, conserva sus costumbres pueblerinas. Una de ellas y la que más te llama la atención: la siesta.");
-alert("Estacionas, te bajas y observas. La casa de dos plantas que alquilaste para pasar los próximos meses parece más confortable que en las fotos. Pero el paisaje es solitario. Apenas unas casas a la vista; la laguna al frente y la brisa cálida de noviembre que anuncia la proximidad del verano. Perfección absoluta. Pero recuerdas la frase que tu madre solía decir antes del accidente: No todo lo que brilla es oro." );
-alert("Comienzas a bajar las cajas, limpiar la casa, ubicar el escritorio con la computadora frente a la ventana que capta la laguna. Será una gran inspiración para escribir. Ya ha pasado más de un año desde la publicación de tu último libro y, gracias a su éxito, la editorial se muestra muy interesada en una nueva obra. El celular suena: es Ana, tu terapeuta.")
+alert(nombre + ", Te encuentras en el puesto del pescadero. Trabajas allí durante el día a cambio de unas monedas. De pronto, escuchas gritos; ves gente, desesperada, corriendo. No entiendes qué está sucediendo.");
+alert("No sabes de qué escapan. ¿Sería lo sensato correr con ellos? De pronto, un rugido amorfo, inhumano, te sobresalta. Ese ruido, sin dudas, no es de este mundo. Te escondes detrás del puesto y ves cómo una lluvia de sangre anticipa la entrada de una siniestra criatura, que mastica los restos de un cuerpo sin vida. Comienza a olfatear y, como si detectara el miedo, clava sus profundos y ovalados ojos negros en ti. Corre en tu dirección, impulsándose con sus garras." );
 
-let accion1 = prompt("Si quieres atender el llamado, ingresa el número 1. Si quieres ignorar la llamada y salir a recorrer el lugar, ingresa el número 2.")
-
+let accion1 = prompt("Si crees que lo mejor es correr, ingresa 1. Si eliges tomar la cuchilla del pescadero y hacerle frente, ingresa 2")
 
 
-if (accion1 == 1) {
-    alert("«- Hola, " + nombre + ". ¿Cómo estás? ¿Ya te instalaste?». Le dices que sí. Le cuentas un poco del viaje, de lo poco que captaste del lugar. « No quería molestarte mucho hoy, pero recuerda que debemos tener la sesión en estos días ». Programan fecha para mañana y se despiden. Te sientas en la cama y piensas. La mirada perdida, de pronto, se encuentra en la foto familiar. Papá, mamá, Clara y tú. Piensas que el universo es un patético bufón, de esos que creen que son elocuentes, pero que en el fondo son siniestros. Se ríe de tí, seguramente, con una carcajada grave y sonora. Y te sonríes al pensar que tú también te reirías de ti: le pagas a alguien para revolver toda la porquería que llevas adentro.");
-    
-} else if (accion1 == 2) { 
-    alert("Hoy, no. Ya has hablado suficiente sobre la muerte de mamá y papá. El cambio de aire es positivo ,y si decidiste venir hasta aquí para dejar todo atrás, no vas a ocapar este primer día con la oscuridad del pasado. Decides dejar el celular sonando en el escritorio y sales. Aún hay sol, faltarán unas horas para que empiece a oscurecer. Caminas hacia la ruta que rodea a la laguna. Escuchas a los pájaros cantar y observas unas sutiles olas en el agua. La brisa se volvió un poco más densa. Algunas personas en bicicleta o corriendo cruzan el camino. A lo lejos ves movimiento en el agua. Observas mejor. Parece una persona agitando los brazos. Comienzas a correr con desesperación hacia la orilla... " );
-    
-} else {
-     while (accion1 != 1 && accion1 != 2) {
-        alert("No ingresaste una opción válida.");
-        accion1 = prompt("Si quieres atender el llamado, ingresa el número 1. Si quieres ignorar la llamada y salir a recorrer el lugar, ingresa el número 2.");
-        
-     }
+
+
+
+while ((accion1 != 1) || (accion1 != 2) ) {
+    if (accion1 == 1) {
+        alert("Corres como si no hubiera un mañana justo cuando la criatura se lanza sobre el puesto. Miras hacia atrás mientras huyes y ves cómo, furiosa, se quita los restos de madera y tripas marinas de encima, al tiempo que reproduce otro fuerte rugido. Te sumerges en la profundidad del bosque, teniendo la esperanza de encontrar a alguien más. De poder respirar y comprender qué es lo que está sucediendo.");
+        break;
+
+    } else if (accion1 == 2) {
+        alert("Tomas la cuchilla rápidamente, pero la criatura vuela de un golpe a ti y al puesto. Caes al suelo y sientes un fuerte dolor en el estómago. Ves que la criatura se acerca lentamente. Piensas en tomar la cuchilla para defenderte... ¿En dónde está? El dolor en el estómago es más punzante. Te tocas. Ahí está la cuchilla. Apenas puedes moverte y la criatura ya está olfateando la herida. Sientes sus garras expandiéndola y cómo la carne cede por completo a la brutalidad, mientras encomiendas tu alma a los dioses. " );
+        break;
+
+    } else {
+        alert("No ingresaste una opción válida. ¡No es momento de dudar!");
+        accion1 = prompt("Si crees que lo mejor es correr, ingresa 1. Si eliges tomar la cuchilla del pescadero y hacerle frente, ingresa 2");
+
+    }
 }
-
-
-
-
-
-
-
